dfdfa4d58e914152be31c0a21878eede38a6e552
hanna 🍌 <hanna@iamhanna.de>
2022-05-03 20:16:23 +0200

    dotenv setup for vercel, add ThemeContext for dark mode

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,12 @@
 import type {LinksFunction, MetaFunction} from '@remix-run/node';
 import {Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration} from '@remix-run/react';
-import {useEffect, useState} from 'react';
+import {FC, useContext} from 'react';
 import styles from './styles/app.css';
 import {RiSunFill, RiSunLine} from 'react-icons/ri';
+import {ThemeContext, ThemeContextProvider} from '~/ThemeContext';
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
-  title: 'New Remix App',
   viewport: 'width=device-width,initial-scale=1',
 });
 
@@ -17,32 +17,48 @@ export const links: LinksFunction = () => {
   ];
 };
 
-export default function App() {
-  const [{system, userSelection}, setDarkMode] = useState<{system: boolean; userSelection: boolean}>({
-    system: true,
-    userSelection: true,
-  });
-  useEffect(() => {
-    const isDarkMode = window && window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-    setDarkMode({system: isDarkMode, userSelection: isDarkMode});
-  }, []);
-
+const Layout: FC = ({children}) => {
+  const {darkMode, switchDarkMode} = useContext(ThemeContext);
   return (
-    <html lang="en" className={userSelection ? 'dark' : ''}>
+    <div>
+      <header>
+        <nav>NAVIGATION</nav>
+        <button onClick={switchDarkMode}>{darkMode.userSelection ? <RiSunLine /> : <RiSunFill />}</button>
+      </header>
+      {children}
+    </div>
+  );
+};
+
+const Document: FC = ({children}) => {
+  const {darkMode} = useContext(ThemeContext);
+  return (
+    <html lang="en" className={darkMode.userSelection ? 'dark' : ''}>
       <head>
         <Meta />
+        <link rel="icon" href={darkMode.system ? 'dog_light.svg' : 'dog.svg'} />
         <Links />
-        <link rel="icon" href={system ? 'dog_light.svg' : 'dog.svg'} />
       </head>
       <body className="bg-white dark:bg-black text-black dark:text-white">
-        <button onClick={() => setDarkMode({system, userSelection: !userSelection})}>
-          {userSelection ? <RiSunLine /> : <RiSunFill />}
-        </button>
-        <LiveReload />
-        <Outlet />
+        {process.env.NODE_ENV === 'development' && <LiveReload />}
+        {children}
         <ScrollRestoration />
         <Scripts />
       </body>
     </html>
   );
-}
+};
+
+const App: FC = () => {
+  return (
+    <ThemeContextProvider>
+      <Document>
+        <Layout>
+          <Outlet />
+        </Layout>
+      </Document>
+    </ThemeContextProvider>
+  );
+};
+
+export default App;
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,8 +1,16 @@
-export default function Index() {
+import {MetaFunction} from '@remix-run/node';
+
+export const meta: MetaFunction = () => ({
+  title: 'i am hanna ///',
+});
+
+const Index: FC = () => {
   return (
     <div>
       <h1>Welcome to Remix</h1>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
-}
+};
+
+export default Index;
