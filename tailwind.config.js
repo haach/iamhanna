b64cea69ee17ef1460f635db343285496bb02ede
hanna 🍌 <hanna@iamhanna.de>
2022-05-05 11:56:47 +0200

    Extend tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: '#f2b11b',
+        yellow: '#f2c608',
+        gray: '#dcdcdc',
+        'gray-dark': '#30383b',
+      },
+      fontFamily: {
+        sans: ['Roboto Condensed', 'sans-serif'],
+        serif: ['Playfair Display', 'serif'],
+      },
+    },
   },
   plugins: [],
   darkMode: 'class',
