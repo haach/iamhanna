2023f0d9a0e63b1f942965dc2a66c7cd8a51a78a
hanna 🍌 <hanna@iamhanna.de>
2022-05-18 20:27:45 +0200

    Add console messge

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,8 @@
-import { RemixBrowser } from "@remix-run/react";
-import { hydrate } from "react-dom";
+import {RemixBrowser} from '@remix-run/react';
+import {hydrate} from 'react-dom';
+
+console.log(
+  '\n🐕 🐕 🐕\n\nHey fellow developer!\n\nI appreciate you taking a look at this!\nIf you find a nasty bug, please drop me a message at https://iamhanna.de/contact\n\nCheers!🍻'
+);
 
 hydrate(<RemixBrowser />, document);
